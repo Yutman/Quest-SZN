689d09e277ee3fadc8f2a6d7379edd29fafa5477
Yutman <kuriapeter95@gmail.com>
2024-05-17 15:06:09 +0300

    barebone structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  return ( 
+  <div className="categories-container">
+  <div className="category-container">
+    {/* <img/> */}
+  <div className="category-body-container">
+    <h2>Laptops</h2>
+    <p>Shop Now</p>
+  </div>
+  </div>
+  <div className="category-container">
+    {/* <img/> */}
+  <div className="category-body-container">
+    <h2>Phones</h2>
+    <p>Shop Now</p>
+  </div>
+  </div>
+  <div className="category-container">
+    {/* <img/> */}
+  <div className="category-body-container">
+    <h2>Accessories</h2>
+    <p>Shop Now</p>
+  </div>
+  </div>
+  <div className="category-container">
+    {/* <img/> */}
+  <div className="category-body-container">
+    <h2>Desktops</h2>
+    <p>Shop Now</p>
+  </div>
+  </div>
+  <div className="category-container">
+    {/* <img/> */}
+  <div className="category-body-container">
+    <h2>Monitors</h2>
+    <p>Shop Now</p>
+  </div>
+  </div>
+  </div>
   );
-}
+};
 
 export default App;

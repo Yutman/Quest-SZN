8db425f23fb37c855522ffc2e23e6ad9a82d6743
Yutman <kuriapeter95@gmail.com>
2024-05-17 15:42:02 +0300

    Setup to convert our data in JS into JSX UI

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Laptops',
+      image: 'https://cdn.thewirecutter.com/wp-content/media/2022/10/laptopstopicpage-2048px-2102-3x2-1.jpg?auto=webp&quality=75&crop=3:2&width=1024',
+    },
+    {
+      id: 2,
+      title: 'Phones',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-QoWQUGvTHv2S150JNC9Ml5cf6EYfFWA-NXj1q1Gszw&s',
+    },
+    {
+      id: 3,
+      title: 'Accessories',
+      image: 'https://5.imimg.com/data5/SELLER/Default/2021/4/US/FX/DC/119904249/electronic-gadgets-500x500.jpg',
+    },
+    {
+      id: 4,
+      title: 'Bags',
+      image: 'https://cyberguy.com/wp-content/uploads/2023/10/CGO-10-best-laptop-bags.jpg',
+    },
+    {
+      id: 5,
+      title: 'Monitors',
+      image: 'https://images.pexels.com/photos/1181675/pexels-photo-1181675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ]
+
   return ( 
   <div className="categories-container">
-  <div className="category-container">
-    {/* <img/> */}
+    {categories.map(({title}) =>  (
+    <div className="category-container">
+    <div className="background-image"/>
   <div className="category-body-container">
-    <h2>Laptops</h2>
+    <h2>{title}</h2>
     <p>Shop Now</p>
   </div>
   </div>
-  <div className="category-container">
-    {/* <img/> */}
-  <div className="category-body-container">
-    <h2>Phones</h2>
-    <p>Shop Now</p>
-  </div>
-  </div>
-  <div className="category-container">
-    {/* <img/> */}
-  <div className="category-body-container">
-    <h2>Accessories</h2>
-    <p>Shop Now</p>
-  </div>
-  </div>
-  <div className="category-container">
-    {/* <img/> */}
-  <div className="category-body-container">
-    <h2>Desktops</h2>
-    <p>Shop Now</p>
-  </div>
-  </div>
-  <div className="category-container">
-    {/* <img/> */}
-  <div className="category-body-container">
-    <h2>Monitors</h2>
-    <p>Shop Now</p>
-  </div>
-  </div>
-  </div>
+))}
+ </div>
   );
 };
 

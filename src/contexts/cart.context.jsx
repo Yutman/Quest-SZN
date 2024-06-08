771be760990b60fd39e212ece1d4ae762a4f1fd1
Yutman <kuriapeter95@gmail.com>
2024-06-08 22:47:53 +0300

    Add CartItem component with styles
    - Imported cart-item.styles.scss for styling
    - Created CartItem functional component
    - Destructured name and quantity from cartItem prop
    - Rendered name and quantity within a div

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -3,15 +3,43 @@ import {
     useState
 } from "react";  
 
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === productToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem => 
+            cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        );
+    }
+
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
 }); 
 
-export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false); 
+
+
+
+    export const CartProvider = ({children}) => {
+        const [isCartOpen, setIsCartOpen] = useState(false);
+        const[cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+
+    }
+
     const value = {isCartOpen, setIsCartOpen};
+
+
 
 return ( <CartContext.Provider value={value}>
             {children}

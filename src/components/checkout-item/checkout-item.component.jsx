ec4a0251609d6c7ed3edbe3828b211c4f1454ebc
Yutman <kuriapeter95@gmail.com>
2024-06-12 20:09:11 +0300

    Add remove item functionality and total price calculation to Checkout component
    - Added click handler to remove item from cart in CheckOutItem component
    - Calculated and displayed total price of items in Checkout component
    - Ensured cart items are dynamically updated and rendered

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout-item.styles.scss';
+
+
+const CheckOutItem = ({cartItem}) => {
+        const {name, imageUrl, price, quantity} = cartItem;
+        const {removeItemFromCart} = useContext(CartContext);
+
+        const handleRemoveClick = () => {
+            removeItemFromCart(cartItem);
+        }
+
+        return (
+            <div className='checkout-item-container'>
+                <div className='image-container'>
+                    <img src={imageUrl} alt={`${name}`}/>
+                </div>
+                <span className='name'>{name}</span>
+                <span className='quantity'>{quantity}</span>
+                <span  className='price'>{price}</span>
+                <div className='remove-button' onClick={handleRemoveClick}>&#10005;</div>
+            </div>
+        );
+};
+
+export default CheckOutItem;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,47 +2,41 @@ import {useContext} from 'react';
 
 import {CartContext} from '../../contexts/cart.context';
 
+import CheckOutItem from '../../components/checkout-item/checkout-item.component';
+
 import './checkout.styles.scss';
 
 
 const Checkout = () => {
-    const {cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext);
+    const {cartItems, addItemToCart, removeItemFromCart} = 
+    useContext(CartContext);
 
+    const total = cartItems.reduce((acc, cartItem) => acc + cartItem.quantity * cartItem.price, 0);
 
     return(
         <div className='checkout-container'>
             <div className='checkout-header'> 
                 <div className='header-block'>
-                    
+                    <span>Product</span>
                 </div>
                 <div className='header-block'>
+                    <span>Description</span>
                 </div>
                 <div className='header-block'>
+                    <span>Quantity</span>
                 </div>
                 <div className='header-block'>
+                    <span>Price</span>
                 </div>
                 <div className='header-block'>
+                    <span>Remove</span>
                 </div>
             </div>
-            <h1>Checkout Page</h1>
-            <div>
-                {
-                cartItems.map((cartItem) => {
-                        const {name, id, quantity} = cartItem;
-                    return ( 
-                            <div key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        <br/>
-                        <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                        <br/>
-                        <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                        <br/>
-                            </div>
-                    );
-                })}
+                {cartItems.map((cartItem) => (
+                            <CheckOutItem key={cartItem.id} cartItem={cartItem} />
+                ))}
+                <span className='total'>Total: ${total}</span>
             </div>
-        </div>
     );
 };
 

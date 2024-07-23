438ceba978f6495a5750b42885833ffa5c549c9c
Yutman <kuriapeter95@gmail.com>
2024-07-23 15:52:34 +0300

    Refactored cart reducer to use action type constants and add setIsCartOpen method

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,8 +1,10 @@
 import { 
-    createContext, 
-    useState, 
-    useEffect 
+    createContext,  
+    useEffect, 
+    useReducer
 } from "react";  
+
+import {createAction} from '../utils/reducer/reducer.utils';
 
 import { 
     createUserDocumentFromAuth,
@@ -15,8 +17,36 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 }); // create a user context object
 
+
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER': 'SET_CURRENT_USER',
+}
+
+const userReducer = (state, action) => {
+    const {type, payload} = action;
+    
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+          return {
+            ...state,
+            currentUser: payload,
+          }
+        default:
+            throw new Error(`Unhandled action type: ${type}`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
 export const UserProvider = ({children}) => {
-const [currentUser, setCurrentUser] = useState(null); 
+const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+console.log(currentUser);
+
+const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+}
 const value = {currentUser, setCurrentUser};
 
 
@@ -35,5 +65,6 @@ useEffect(() => {
             {children}
         </UserContext.Provider>
 }; // create a user provider component that will wrap the entire application and provide the user context object to all components in the application
+
 
 

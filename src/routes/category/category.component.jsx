085d8cef6c11a92b92a80814ba0fc72ed62b4557
Yutman <kuriapeter95@gmail.com>
2024-07-29 14:39:23 +0300

    Demystifying middleware and redux-logger

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -11,10 +11,12 @@ import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useSelector(selectCategoriesMap);
+  console.log('render/re-rendering category component')
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
-
+  
   useEffect(() => {
+    console.log('effect fired calling setProducts')
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,23 @@
 import {compose, createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import {rootReducer} from './root-reducer';
 
-const middleWares = [logger]; // logger middleware
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+
+    console.log('type', action.type);
+    console.log('payload', action.payload);
+    console.log('currentState', store.getState());
+
+    next(action);
+
+    console.log('next state:', store.getState());
+}
+
+const middleWares = [loggerMiddleware]; // logger middleware
 
 const composedEnhancers = compose(applyMiddleware(...middleWares)); // compose the logger middleware
 

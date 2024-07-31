845f2dae516df4b2582ecb6f0e2d87fc0913df55
Yutman <kuriapeter95@gmail.com>
2024-07-31 14:55:09 +0300

    Removed  link

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -7,7 +7,6 @@ import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component
 
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
-
 
 import { ReactComponent as QuestLogo } from '../../assets/quest.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';

10572dc24446183a4aaef2c727caaa79301ffaa5
Yutman <kuriapeter95@gmail.com>
2024-07-31 15:16:37 +0300

    Implemented loggerMiddleware to log action details and state changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,10 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import {rootReducer} from './root-reducer';
 import storage from 'redux-persist/lib/storage';
 
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next(action);
-    }
-
-    console.log('type', action.type);
-    console.log('payload', action.payload);
-    console.log('currentState', store.getState());
-
-    next(action);
-
-    console.log('next state:', store.getState());
-};
 
 const persistConfig = {
     key: 'root',
@@ -28,9 +14,19 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware]; // logger middleware
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+); //  ensures that the logger middleware is only included in the array if the application is running in a development environment
+//In production, the array ends up empty improving performance and avoid leaking potentially sensitive debug information
 
-const composedEnhancers = compose(applyMiddleware(...middleWares)); // compose the logger middleware
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares)); // compose the logger middleware
 
 export const store = createStore(
     persistedReducer, 
